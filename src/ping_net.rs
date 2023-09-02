bb4512c8fea81cac51523ccbadf725575a3ae1e5
Alexander Borodulya <alexander.borodulya@gmail.com>
2023-09-03 01:01:46 +0300

    Fixed PingNet

--- src/ping_net.rs
+++ src/ping_net.rs
@@ -37,17 +37,17 @@ pub struct PingNet;
 
 pub type RecvRespond = (Ipv4Addr, u16);
 
-const IPV4_PACKET_BUFFER_SIZE: usize = EchoReplyPacket::minimum_packet_size() + Ipv4Packet::minimum_packet_size();
+const ECHO_IPV4_PACKET_BUFFER_SIZE: usize = EchoReplyPacket::minimum_packet_size() + Ipv4Packet::minimum_packet_size();
 
 impl PingNet {
     pub fn create_socket() -> Result<Arc<Socket>, NetError> {
-        let sock_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 0).into();
         let socket = Socket::new(
             Domain::IPV4,
-            Type::DGRAM,
+            Type::RAW,
             Some(Protocol::ICMPV4),
         ).map_err(|e| NetError::SocketCreationError(e.to_string()))?;
-        socket.bind(&sock_addr).map_err(|e| NetError::SocketBindError(e.to_string()))?;
+        let ipv4_unspecified: SockAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::UNSPECIFIED), 80).into();
+        socket.bind(&ipv4_unspecified).map_err(|e| NetError::SocketBindError(e.to_string()))?;
         Ok(Arc::new(socket))
     }
 
@@ -55,7 +55,7 @@ impl PingNet {
         let addr = &connection_handle.destination;
         let seq_num = connection_handle.seq;
         let identifier = connection_handle.identifier;
-
+        
         log::debug!("send_ping_request: seq_num: {} - started", seq_num);
 
         let mut buf = [0u8; MutableEchoRequestPacket::minimum_packet_size()];
@@ -68,36 +68,54 @@ impl PingNet {
     }
 
     pub async fn recv_ping_respond(socket: Arc<Socket>) -> Option<RecvRespond> {
+
         log::debug!("recv_ping_respond - started");
 
-        let mut buf: [MaybeUninit<u8>; IPV4_PACKET_BUFFER_SIZE] = [MaybeUninit::<u8>::uninit(); IPV4_PACKET_BUFFER_SIZE];
+        let mut buf: [MaybeUninit<u8>; ECHO_IPV4_PACKET_BUFFER_SIZE] = [MaybeUninit::<u8>::uninit(); ECHO_IPV4_PACKET_BUFFER_SIZE];
         let (size, _sock_addr) = PingNet::read_socket(&socket, &mut buf).await;
 
-        if size > Ipv4Packet::minimum_packet_size() {
+        log::debug!("recv_ping_respond: size: {}, _sock_addr: {:?}", size, _sock_addr.as_socket_ipv4());
+
+        if size >= ECHO_IPV4_PACKET_BUFFER_SIZE {
             let temp: Vec<u8> = buf.iter_mut().map(|b| unsafe { b.assume_init() }).collect();
-            let o: [u8; IPV4_PACKET_BUFFER_SIZE] = match temp.try_into() {
+            let o: [u8; ECHO_IPV4_PACKET_BUFFER_SIZE] = match temp.try_into() {
                 Ok(arr) => arr,
                 Err(_e) => {
-                    log::warn!("recv_ping_respond: Vec<u8>.try_into failed, size: {}, IPV4_PACKET_BUFFER_SIZE: {} - finished with None", size, IPV4_PACKET_BUFFER_SIZE);
-                    return None // TODO: NetError::RecvBufferInitFailed
+
+                    log::warn!("recv_ping_respond: Vec<u8>.try_into failed, size: {}, ECHO_IPV4_PACKET_BUFFER_SIZE: {} - finished with None", size, ECHO_IPV4_PACKET_BUFFER_SIZE);
+                    
+                    // TODO: NetError::RecvBufferInitFailed
+                    return None
                 }
             };
-            
+
             let ipv4_packet = Ipv4Packet::new(&o)?;
+            
             if ipv4_packet.get_next_level_protocol() == pnet::packet::ip::IpNextHeaderProtocols::Icmp {
                 let icmp_packet = EchoReplyPacket::new(ipv4_packet.payload())?;
-                if icmp_packet.get_icmp_type() == IcmpTypes::EchoReply {
+                
+                let icmp_type = icmp_packet.get_icmp_type();
+                let source = ipv4_packet.get_source();
+                let destination = ipv4_packet.get_destination();
+                
+                if icmp_type == IcmpTypes::EchoReply || (icmp_type == IcmpTypes::EchoRequest && source == Ipv4Addr::new(127, 0, 0, 1) && destination == Ipv4Addr::new(127, 0, 0, 1)) {
                     let ipv4_addr_recv = ipv4_packet.get_source();
                     let seq_num_recv = icmp_packet.get_sequence_number();
                     let idetifier = icmp_packet.get_identifier();
+
                     log::debug!("recv_ping_respond: ip: {}, seq_num_recv: {}, idetifier: {}", ipv4_addr_recv, seq_num_recv, idetifier);
+
                     return Some((ipv4_addr_recv, seq_num_recv));
+                } else {
+                    log::warn!("recv_ping_respond: Unexpected ICMP type: {:?}", icmp_packet.get_icmp_type());
                 }
             }
         }
-        log::warn!("recv_ping_respond: size={}, Ipv4Packet::minimum_packet_size()={}, finished with None", size, Ipv4Packet::minimum_packet_size());
-        None // TODO: NetError::RecvNotEchoReply
-    
+
+        log::warn!("recv_ping_respond: size={}, ECHO_IPV4_PACKET_BUFFER_SIZE={}, finished with None", size, ECHO_IPV4_PACKET_BUFFER_SIZE);
+        
+        // TODO: NetError::RecvNotEchoReply
+        None
     }
 
     ///
@@ -150,23 +168,21 @@ mod tests {
 
     #[tokio::test]
     async fn test_send_recv_icpm_echo() {
-        let socket = PingNet::create_socket().expect("Socket creation failed");
-
         let destination_ipv4 = Ipv4Addr::new(127, 0, 0, 1);
         let seq = 1;
         let identifier = 1;
         let earlier = Instant::now();
         let now = Instant::now();
         let status = EchoStatus::Pending;
-
+        
         let connection_handle = ConnectionHandle::new(destination_ipv4, seq, identifier, earlier, now, status);
+        
+        let socket = PingNet::create_socket().expect("create_socket failed");
 
-        let send_result = PingNet::send_ping_request(Arc::clone(&socket), connection_handle).expect("Send request failed");
+        let send_result = PingNet::send_ping_request(Arc::clone(&socket), connection_handle).expect("send_ping_request failed");
         assert_eq!(send_result, ());
         
-        let socket = Arc::clone(&socket);
-        
-        let recv_result = PingNet::recv_ping_respond(socket.clone()).await;
-        assert_eq!(recv_result, Some((destination_ipv4, seq)));
+        let recv_result = PingNet::recv_ping_respond(Arc::clone(&socket)).await.expect("recv_ping_respond failed");
+        assert_eq!(recv_result, (destination_ipv4, seq));
     }
 }
