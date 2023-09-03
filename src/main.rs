e83c84b5b2866176afdbf565606fee3b83f72c02
Alexander Borodulya <alexander.borodulya@gmail.com>
2023-09-03 17:41:55 +0300

    Break down fn run into smaller functions. Make PingApp as a struct.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use clap::Parser;
-use ping_template::{cli_args::CliArgs, ping_app};
+use ping_template::{cli_args::CliArgs, ping_app, utils};
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
     console_subscriber::init();
-    ping_app::run(CliArgs::parse()).await;
+    
+    let ping_app = ping_app::PingApp::new(CliArgs::parse());
+    if let Err(err) = ping_app.run().await {
+        log::error!("Application error: {}", err);
+    }
+
+    utils::dump_conn_map(ping_app.conn_map()).await;
 }
